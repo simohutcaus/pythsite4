81aaee68278b865de951329c719ed9cb23ba677e
Chocolol <simonhutc@gmail.com>
2022-02-21 14:49:15 +1100

    keygone

--- mysite/settings.py
+++ mysite/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-efdgixe91-u607a#q2g%qklzha#qr$78nrf@4l$4pgrmssz8@h'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
